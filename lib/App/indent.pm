734ab815a3ecb3f2e46d268373b42b49f0798eb1
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2024-01-25 11:41:27 +0700

    Initial commit

---
+++ lib/App/indent.pm
@@ -0,0 +1,24 @@
+package App::indent;
+
+use strict;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Indent text
+
+=for Pod::Coverage ^(.+)$
+
+=head1 SYNOPSIS
+
+See the included command-line script L<indent>.
+
+
+=head1 SEE ALSO
+
+L<String::Indent>
+
+=cut
